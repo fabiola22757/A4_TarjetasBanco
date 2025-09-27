1d8b9bacd8997fe91ecf1e1db7241277a075ff9f
Fabiola <fabiola.juarez>
2025-09-26 18:13:22 -0600

    Initial Commit

---
+++ A4_TarjetasBanco/A4_TarjetasBancoApp.swift
@@ -0,0 +1,17 @@
+//
+//  A4_TarjetasBancoApp.swift
+//  A4_TarjetasBanco
+//
+//  Created by win603 on 26/09/25.
+//
+
+import SwiftUI
+
+@main
+struct A4_TarjetasBancoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
